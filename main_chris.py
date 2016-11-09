50a56ab5c891628312163755c4aeb05fc69589fc
christian <chriiscardozo@gmail.com>
2016-11-09 10:42:57 -0200

    creating svd option

---
+++ main_chris.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+#from GOTTESI import *
+from Util import TextProcess
+from Util import TESIUtil
+from NamedEntities.KnowledgeExtractor import KnowledgeExtractor
+from NamedEntities.NamedEntity import NamedEntitiesDict
+from TFIDF.TFIDF import TFIDFCalculator
+import sys
+
+def main():
+	epi_dir = 'episodes'
+	epi_pre_dir = 'episodes_preproc'
+	entities_file = 'entities.csv'
+	relationships_file = 'relationships.csv'
+
+	if('preprocess' in sys.argv):
+		print('pre processing text... ', end='')
+		TextProcess.pre_process_got_base(epi_dir, epi_pre_dir)
+		print('preprocess ok')
+
+	if('find_ne' in sys.argv):
+		print('identifying named entities...')
+		extractor = KnowledgeExtractor(epi_pre_dir)
+		tagged = extractor.find_entities(entities_file)
+		extractor.find_relationships(tagged, relationships_file)
+		print('find_ne ok')
+
+	if('tfidf' in sys.argv or 'query' in sys.argv):
+		print('calculating tfidf...')
+		tfidf_calc = TFIDFCalculator(epi_pre_dir, entities_file)
+		tfidf_calc.calculateTFIDF()
+		print('tfidf ok')
+
+		if('query' in sys.argv):
+			tfidf_calc.query('svd' in sys.argv)
+
+	# test code
+	if('merge_ne' in sys.argv):
+		print('loadig named entities from file...')
+		dic = NamedEntitiesDict.load_entities_dict_from_file(epi_pre_dir, entities_file)
+		lst = TESIUtil.dict_to_list(dic)
+		final = 0
+		for item1 in lst:
+			for item2 in lst:
+				if(item1 == item2):
+					continue
+				summ = 0
+				qty = 0
+				for str1 in item1.terms():
+					str1 = TESIUtil.remove_honor_words(str1)
+					for str2 in item2.terms():
+						str2 = TESIUtil.remove_honor_words(str2)
+						summ += TESIUtil.string_similarity(str1, str2)
+						qty += 1
+				avg = summ/qty
+				if(avg > 0.7):
+					print(str(avg) + " => " + str(item1.terms()) + " & " + str(item2.terms()))
+					final += 1
+		print(final)
+
+
+main()
